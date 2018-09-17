cf498fa914ace57a6d796a05809acb9892000d47
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-17 06:37:44 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize!}:"
  holiday.each do |holiday, supply|
-      puts "  #{holiday.to_s.split("_").collect(&:capitalize!).join" "}: #{supply.join ", "}"
+      puts "  #{holiday.to_s.split("_").collect { || }.join" "}: #{supply.join ", "}"
 end
 end
 end
